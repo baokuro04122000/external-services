eba72561ca595a678aefb826cb6369aa5f03f5e2
baotran13 <bao.tran@techupcorp.com>
2022-11-12 21:01:20 +0700

    fix<cluster>: pm2 error

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,6 @@ module.exports = {
         name: "external",
         script: "./server.js",
         exec_mode: 'cluster',
-        instances: 2,
-        watch: true,
-        ignore_watch: ["node_modules"],
-        env_production: {
-            NODE_ENV: "production"
-        },
-        env_development: {
-            NODE_ENV: "development"
-        }
+        instances: 2
     }]
 }
